1170e2b61258742cb3d2a61da334b8029f679d5e
yohannmarchand <marchandyohannnnn@gmail.com>
2022-02-16 20:56:30 +0100

    add style to component

---
+++ src/VerticalList.style.js
@@ -0,0 +1,20 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    root: {
+        listStyle: "none",
+        display: "flex",
+        flexWrap: "wrap",
+        margin: 0,
+        padding: 0,
+        justifyContent: "center",
+    },
+    item: {
+        padding: 8,
+    },
+    link: {
+        textDecoration: "none",
+    },
+});
+
+export default useStyles;
